134e9ff2c0ff6009f144a401a0c6bbb8cd00c2c4
Haikalin <haikalassyauqi70@gmail.com>
2024-06-27 16:47:58 +0700

    fix table

--- kalsong/src/frontend/footer.jsx
+++ kalsong/src/frontend/footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
     return (
-        <div class="w-1/2 mx-auto bottom-0 h-8 fixed inset-x-0 lg:h-16 flex justify-center align-middle items-center">
+        <div class="w-1/2 mx-auto bottom-0 h-16 fixed inset-x-0 lg:h-16 flex justify-center align-middle items-center">
             <h1 class="text-base text-white">Made by: Haikal Assyauqi</h1>
         </div>
     );
