9343aa308808de93d32846ee4363d77830bcea25
Haikalin <haikalassyauqi70@gmail.com>
2024-07-14 16:59:26 +0700

    fix list

--- kalsong/src/frontend/tabelLagu.jsx
+++ kalsong/src/frontend/tabelLagu.jsx
@@ -10,6 +10,7 @@ export const TabelLagu = ({data, num}) => {
                 <div key={index+num} className="flex-shrink-0 p-1.5 basis-1/5 max-w-[20%]" >
                     <div id="containerImage" className="w-full mx-auto">
                         <img src={song.track.album.images[0].url} className="mx-auto rounded-lg lg:w-24" alt="" />
+                        <link as="image" href={song.track.album.images[0].url} className="mx-auto rounded-lg lg:w-24" alt="" rel="preload" />
                     </div>
                     <div className="relative w-full text-xxs md:text-xs text-clip text-center text-white lg:text-sm wrap whitespace-nowrap overflow-hidden justify-center">{`${index+1+num}. ${song.track.name.split(/ [-/()]/)[0]}`}</div>
                     <div className="relative w-full text-xxs md:text-xs text-clip text-center text-white lg:text-xs wrap whitespace-nowrap overflow-hidden">{song.track.artists[0].name === "GAC (Gamaliél Audrey Cantika)" ? "GAC" : song.track.artists[0].name}</div>
